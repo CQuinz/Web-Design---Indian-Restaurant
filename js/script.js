00f632400efc35016819306bd033920c3dd9d948
Christian <christian.quinlan@yahoo.co.uk>
2016-12-17 18:17:39 +0000

    changes to carosel added header tags

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ $( document ).ready(function() {
     var $slider = $('.slider'); // class or id of carousel slider
     var $slide = 'li'; // could also use 'img' if you're not using a ul
     var $transition_time = 1000; // 1 second
-    var $time_between_slides = 4000; // 4 seconds
+    var $time_between_slides = 5000; // 4 seconds
   
     function slides(){
       return $slider.find($slide);
